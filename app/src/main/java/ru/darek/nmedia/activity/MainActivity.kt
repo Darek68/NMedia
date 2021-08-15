c3c84d15fe00db995084abca152eaaff6bddc972
Darek <darek68@list.ru>
2021-08-16 00:04:51 +0300

    IntentsBed

--- app/src/main/java/ru/darek/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/darek/nmedia/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.darek.nmedia.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.launch
 //import android.text.TextUtils
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +15,7 @@ import ru.darek.nmedia.databinding.ActivityMainBinding
 import ru.darek.nmedia.dto.Post
 import ru.darek.nmedia.util.AndroidUtils
 import ru.darek.nmedia.viewmodel.PostViewModel
+import ru.netology.nmedia.activity.NewPostResultContract
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,19 +35,29 @@ class MainActivity : AppCompatActivity() {
             override fun onLike(post: Post) {
                 viewModel.likeById(post.id)
             }
+          /*  override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+            } */
             override fun onShare(post: Post) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
                 viewModel.shareById(post.id)
             }
-
         })
         binding.list.adapter = adapter
 
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
-            binding.group.visibility = View.GONE // перестаёт занимать место на экране
+           // binding.group.visibility = View.GONE // перестаёт занимать место на экране
             //binding.group.visibility = View.INVISIBLE // невидима, но занимает место на экране
         }
-        viewModel.edited.observe(this) { post ->
+     /*   viewModel.edited.observe(this) { post ->
             if (post.id == 0L) {
                 return@observe
             } else binding.group.visibility = View.VISIBLE
@@ -83,8 +96,14 @@ class MainActivity : AppCompatActivity() {
             binding.content.clearFocus()
             AndroidUtils.hideKeyboard(binding.content)
             binding.group.visibility = View.GONE // перестаёт занимать место на экране
-            //binding.group.visibility = View.INVISIBLE // невидима, но занимает место на экране
-
+        } */
+        val newPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+            viewModel.save()
+        }
+        binding.fab.setOnClickListener {
+            newPostLauncher.launch()
         }
     }
 }
